d5a11c64b28eeaa55aab4065d32b4972ce9d2624
Aakash Verma <akashverma611081@gmail.com>
2024-08-27 15:24:11 +0530

    core-error

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://charming-taiyaki-9b9c4e.netlify.app",
+    ],
     credentials: true,
   })
 );
