b1b08fe52abbb47b0e2f03748552d02844f00a3b
Aakash Verma <akashverma611081@gmail.com>
2024-08-28 00:03:12 +0530

    front end url changed2

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
-// const dotenv = require("dotenv").config();
+// Load environment variables from .env file
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// Import routes and middleware
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const contactRoute = require("./routes/contactRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
-const cookieParser = require("cookie-parser");
-const path = require("path");
 
 const app = express();
 
-// Middlewares
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// CORS middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://invt-app.netlify.app/"],
+    origin: ["http://localhost:3000", "https://invt-app.netlify.app"],
     credentials: true,
   })
 );
 
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Static folder for uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routes Middleware
+// Routes
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/contactus", contactRoute);
 
-// Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
-// Error Middleware
+// Error middleware
 app.use(errorHandler);
-// Connect to DB and start server
+
+// Connect to the database and start the server
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -52,4 +58,4 @@ mongoose
       console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error(err));
